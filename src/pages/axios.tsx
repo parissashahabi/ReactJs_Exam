14e522e0d85990c18ce053bdcbe40409e3fe87ce
parisa <prisashahbi@gmail.com>
2021-07-20 13:17:01 +0430

    fetch data with Axios

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -1,16 +1,40 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { IUser } from "../types/user";
 // you don't need anything else to import
 
 interface Props {
   // TODO: add user type
+  items: IUser;
 }
-
-export default function AxiosTest(props: Props) {
+const AxiosTest: React.FC<Props> = (props) => {
   // TODO: once you get data map through data and show them name
-  return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
-}
+  const [users, setUserList] = useState<Props[]>([]);
+  const getRepo = () => {
+    axios.get<Props[]>("/api/users").then((response) => {
+      console.log(response);
+      setUserList(response.data);
+    });
+  };
+  useEffect(() => getRepo(), []);
+  console.log(users[0]);
+  return (
+    <>
+      <div>
+        <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>
+        <ul>
+          {users.map((user) => (
+            <li>
+              <span>{user.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
+export default AxiosTest;
 // TODO: fetch data with axios to use in app
 // RestApi: axios.get("/api/users") => [{name: "amir"}]
 // NOTE: data fetching should happen in server side
